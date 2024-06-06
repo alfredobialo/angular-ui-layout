b97478af1d2f4af9f4b7499ecf5869991a5cdf0d
Alfred Obialo <alfredcsdinc@gmail.com>
2024-06-06 16:23:12 +0100

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,79 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'aui-app',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <div class="p-3">
+      <h1>asom.rental.ui</h1>
+      <p>App Title: {{title}}</p>
+
+      <pre>
+        <table class="table" border="0" cellpadding="5" cellspacing="0">
+          <tr style="font-weight: bolder">
+            <td style="width:4%;">S/N</td>
+            <td style="width:13%;  text-align:left">First Name</td>
+            <td style="width:13%;  text-align:left;">Last Name</td>
+            <td style="width:10%;  text-align:left;">Email</td>
+            <td style="width:5%;  text-align:left;">Age</td>
+          </tr>
+          @for(student of students; track student.id) {
+          <tr [class]="{'bg-gray' : $odd}">
+            <td>{{$index + 1}}</td>
+            <td>{{student.firstName}}</td>
+            <td>{{student.lastName}}</td>
+            <td>{{student.email}}</td>
+            <td>{{student.age}}</td>
+          </tr>
+          }
+        </table>
+
+      </pre>
+    </div>
+
+  `
 })
 export class AppComponent {
-  title = 'biznes-book';
+  title = 'Biznes-Book';
+  students  = [
+    {
+    firstName : "James",
+    lastName : "Okoye",
+    email : "jameso@yahoo.com",
+    age : 34,
+    id:"james-okoye"
+  },
+    {
+      firstName : "Chinoye",
+      lastName : "Alvana",
+      email : "chialvana@yahoo.com",
+      age : 18,
+      id:"chi-alvana"
+    },{
+      firstName : "Valentine",
+      lastName : "Uche",
+      email : "valuche@yahoo.com",
+      age : 18,
+      id:"chi78-alvana"
+    },{
+      firstName : "Peter",
+      lastName : "Odili",
+      email : "419@yahoo.com",
+      age : 18,
+      id:"chi56-alvana"
+    },{
+      firstName : "Chinonso",
+      lastName : "Alvana",
+      email : "chialvana@yahoo.com",
+      age : 18,
+      id:"chi2-alvana"
+    },{
+      firstName : "Dumebi",
+      lastName : "Alvana",
+      email : "chialvana@yahoo.com",
+      age : 18,
+      id:"chi3-alvana"
+    },
+  ]
 }
